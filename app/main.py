7980b4ad5761b4ca341ce6091f4ee0e301bee70b
Trinanjan Nandi <trino.nandi@gmail.com>
2026-04-14 00:55:59 -0700

    add core endpoint

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"message": "Hello, FastAPI with uv!"}
+
+
+@app.get("/")
+def core():
+    return {"message": "This is the core endpoint"}
